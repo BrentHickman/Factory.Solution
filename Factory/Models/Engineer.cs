c354600825584a2f71935bea9e80fd16622653b8
BrentHickman <brenthickman@ymail.com>
2023-03-17 10:39:27 -0700

    added validation to Engineer and Machine Model fields.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,8 +7,10 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The Engineer's Name field can't be empty!")]
     public string EngineerName { get; set; }
-
+    
+    [Required(ErrorMessage = "The Engineer's Hire Date field can't be empty!")]
     [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:M/d/yyyy}")]
     public DateTime HireDate { get; set; }
     public List<EngMach> JoinEntities { get; }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,8 +7,11 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "The Machine's Name field can't be empty!")]
     public string MachineName { get; set; }
 
+    [Required(ErrorMessage = "The Machine's Install Date field can't be empty!")]
     [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:M/d/yyyy}")]
     public DateTime InstallDate { get; set; }
 
